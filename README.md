cb2062534d359f66496c6bd4b2acbd3531f1a933
Fabian Stäber <fabian@fstab.de>
2025-07-03 14:43:31 +0200

    Final version for the Grafana & Friends Meetup

--- README.md
+++ README.md
@@ -1,77 +1,15 @@
 # Zero Code Instrumentation with eBPF
 
-## Run the Demo
+The following script will create a kind cluster and deploy the demo there.
 
-### Planet Service (Java)
+./scripts/run.sh
 
-Build
+Now, run the following command to expose port 3000 of the Grafana Pod on localhost:
 
-```
-cd code/planet-service
+kubectl port-forward $(kubectl get pods -lapp=grafana -o=name) 3000:3000
 
-./mvnw package
-```
+Access Grafana on http://localhost:3000. Default username is admin with password admin.
 
-Run
+Click on Dashboards (menu on the left) -> RED metrics (list of dashboards) to view an example RED metrics dashboard.
 
-```
-cd code/planet-service/
-
-export OTEL_SERVICE_NAME=planet-service
-java -jar target/planet-service-0.0.1-SNAPSHOT.jar
-```
-
-### Greeting Service (Python)
-
-Build
-
-```
-cd code/greeting-service/
-
-python3 -m venv venv
-source venv/bin/activate
-python -m pip install flask
-```
-
-Run
-
-```
-cd code/greeting-service/
-
-export OTEL_SERVICE_NAME=greeting-service
-source ./venv/bin/activate
-./venv/bin/python -m flask --app greeting-service run
-```
-
-### Frontend (NodeJS)
-
-Run
-
-```
-cd code/frontend
-
-export OTEL_SERVICE_NAME=frontend
-node ./frontend.js
-```
-
-### Client
-
-Run
-
-```
-curl localhost:8081
-```
-
-### OTel LGTM Backend
-
-```
-docker run -P 3000:3000 -P 4317:4317 -P 4318:4318 grafana/otel-lgtm:latest
-```
-
-### Beyla
-
-```
-export BEYLA_OPEN_PORT=8080,8081,5000
-export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317
-sudo -E ./beyla
-```
+Click on Explore (menu on the left) to explore Prometheus metrics, Tempo traces, and the Tempo service graph.
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 
-make
+set -e
 
-kind create cluster
-kind load docker-image beyla-demo/planet-service
-kind load docker-image beyla-demo/greeting-service
-kind load docker-image beyla-demo/frontend
-kind load docker-image fstab/beyla
+if ! kind create cluster ; then
+    echo "Error: Kind cluster is already runnning. Please run 'kind delete cluster' before executing this script."
+    exit 1
+fi
 
-kind load docker-image prom/prometheus
-kind load docker-image grafana/tempo
-kind load docker-image grafana/k6
-kind load docker-image grafana/grafana
+for service in frontend greeting-service planet-service ; do
+    echo
+    echo "############################################################"
+    echo "# Building $service"
+    echo "############################################################"
+    cd ./demo-services/$service
+    ./build.sh
+    cd ../..
+    kind load docker-image beyla-demo/$service
+done
 
+echo
+echo "############################################################"
+echo "# Loading upstream Docker images"
+echo "############################################################"
+for docker_image in prom/prometheus grafana/tempo grafana/k6 grafana/grafana grafana/beyla ; do
+    echo "Loading $docker_image"
+    if ! docker image inspect $docker_image >/dev/null 2>&1 ; then
+        docker pull $docker_image
+    fi
+    kind load docker-image $docker_image
+done
+
+echo
+echo "############################################################"
+echo "# Deploying the demo"
+echo "############################################################"
 kubectl create configmap beyla-config --from-file=./deploy/beyla-config.yaml
 kubectl create configmap prometheus-config --from-file=./deploy/prometheus-config.yaml
 kubectl create configmap load-generator --from-file=./deploy/load-generator.js
 kubectl create configmap grafana-provisioning --from-file=./deploy/grafana-datasources.yaml --from-file=./deploy/grafana-dashboards.yaml
 kubectl create configmap grafana-dashboards --from-file=./deploy/grafana-dashboard-red-metrics.json
 kubectl create configmap tempo-config --from-file=./deploy/tempo-config.yaml
-#
+
 kubectl apply \
 	-f ./deploy/planet-service.yaml \
 	-f ./deploy/greeting-service.yaml \
@@ -29,3 +50,8 @@ kubectl apply \
 	-f ./deploy/load-generator.yaml \
 	-f ./deploy/grafana.yaml \
 	-f ./deploy/tempo.yaml \
+
+echo
+echo 'Success!'
+echo 'Please run the following command to expose Grafana on http://localhost:3000'
+echo 'kubectl port-forward $(kubectl get pods -lapp=grafana -o=name) 3000:3000'
